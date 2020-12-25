89fdc470ddcfb31112d0201b56308ab5aa26a136
btsioni <baraktsioni@gmail.com>
2020-12-25 14:40:09 +0200

    first commit

---
+++ src/main/java/com/example/raspberry/RaspberryApplication.java
@@ -0,0 +1,24 @@
+package com.example.raspberry;
+
+import com.pi4j.io.gpio.*;
+
+//@SpringBootApplication
+public class RaspberryApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(RaspberryApplication.class, args);
+		System.out.println("Hello world");
+
+		GpioController controller = GpioFactory.getInstance();
+		GpioPinDigitalOutput pin = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01,"MyLed", PinState.LOW);
+
+		try {
+			for ( int i=0; i<30; i++) {
+				pin.toggle();
+				Thread.sleep(2000);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
