5b989c1eb52679ce10338aea7e431e1ed667027e
btsioni <baraktsioni@gmail.com>
2020-12-25 15:03:38 +0200

    add spring

--- src/main/java/com/example/raspberry/RaspberryApplication.java
+++ src/main/java/com/example/raspberry/RaspberryApplication.java
@@ -1,12 +1,14 @@
 package com.example.raspberry;
 
 import com.pi4j.io.gpio.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class RaspberryApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(RaspberryApplication.class, args);
+		SpringApplication.run(RaspberryApplication.class, args);
 		System.out.println("Hello world");
 
 		GpioController controller = GpioFactory.getInstance();
